6078aca39fbad6bbb129009c88bfb2ef5b2f3d48
Pragnan143 <pragnanmanam8@gmial.com>
2023-04-17 21:01:33 +0530

    changed layout

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,7 +7,7 @@ const RegisterPage = () => {
   return (
     <div className="m-0 p-0 bg-color4 h-screen	w-full flex  items-center justify-center ">
       <div className=" bg-white sm:w-[16rem] lg:h-[35rem] lg:w-[30rem]    rounded-xl flex flex-col overflow-hidden">
-        <div className=" h-50  w-[25rem]  mt-8 mx-10 flex items-center gap-12 pb-5 justify-center relative">
+        <div className="   w-[25rem]  mt-8 mx-10 flex items-center gap-12 p-5 justify-center relative">
           <div
             onClick={() => setMode("login")}
             className="font-semibold   w-[10rem]  flex items-center justify-center cursor-pointer text-[#4B4444]    "
