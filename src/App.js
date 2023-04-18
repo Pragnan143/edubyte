5e2714158af699e83cdafe1632b99eeb54fbfeb1
Pragnan143 <pragnanmanam8@gmial.com>
2023-04-19 00:12:07 +0530

    [.]Dashboard completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import Dashbord from "./components/Dashbord";
 import "./style.css";
 const App = () => {
-  return <div className="App">
-  <Dashbord/>
-  </div>;
+  return (
+    <div className="App">
+      <Dashbord />
+    </div>
+  );
 };
 
 export default App;
