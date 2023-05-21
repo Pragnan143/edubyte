bfa57faeb31c5775f89bea66f478377a00709d3a
Pragnan143 <pragnanmanam8@gmial.com>
2023-05-21 14:46:14 +0530

    [.]Apply Button Added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,20 @@
 import React from "react";
-import CloseIcon from "@mui/icons-material/Close";
 import "react-responsive-modal/styles.css";
-import { Modal } from "react-responsive-modal";
-const SignUpPage = () => {
-  const [open, setOpen] = React.useState(false);
 
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+// import CloseIcon from "@mui/icons-material/Close";
+// import { Modal } from "react-responsive-modal";
+// import { Password } from "@mui/icons-material";
+const SignUpPage = () => {
+  // const [open, setOpen] = React.useState(false);
+  const [uname, setUsername] = React.useState("");
+  const [email, setemail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  console.log(uname);
+  console.log(email);
+  console.log(password);
+
+  // const onOpenModal = () => setOpen(true);
+  // const onCloseModal = () => setOpen(false);
 
   return (
     <form
@@ -22,12 +30,14 @@ const SignUpPage = () => {
       <input
         required
         type="text"
+        onChange={(e) => setUsername(e.target.value)}
         placeholder="Your Name Buddy..."
         className="border-b-2 font-light   w-[18rem] lg:w-[20rem] px-4 py-2 outline-none   "
       />
       <input
         required
         type="email"
+        onChange={(e) => setemail(e.target.value)}
         placeholder="Email..."
         className="border-b-2 font-light  w-[18rem] lg:w-[20rem] px-4 py-2 outline-none   "
       />
@@ -35,12 +45,12 @@ const SignUpPage = () => {
         required
         min={6}
         type="password"
+        onChange={(e) => setPassword(e.target.value)}
         placeholder="Password Dude..."
         className="  border-b-2 outline-none  w-[18rem] lg:w-[20rem] p-1 font-light "
       />
       <button
         type="button"
-        onClick={onOpenModal}
         className="absolute h-35  w-[130%] lg:w-[20rem] bottom-5   lg:bottom-0 rounded-lg bg-lime-800 text-white  p-2 justify-center"
       >
         {/* <Modal
